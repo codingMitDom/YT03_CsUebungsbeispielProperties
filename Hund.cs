445434995b1fd0f8cb4b88f2f1aa0bd2d00d920a
dwit.at <137614685+codingMitDom@users.noreply.github.com>
2024-07-19 10:27:41 +0200

    Add files via upload

---
+++ Hund.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsTutorial
+{
+    internal class Hund
+    {
+        // Felder
+        private string _name;
+        private byte _alter;    // Wertebereich von 0 bis 255
+
+
+        // Properties
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))    // Meldung ausgeben bei Leerstring oder null
+                {
+                    Console.WriteLine("Name darf nicht leer oder null sein!");
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+
+
+        public byte Alter  // Wertebereich von 0 bis 255
+        {
+            get
+            {
+                return _alter;
+            }
+            set
+            {
+                if (value > 50) // maximales Alter ist 50
+                {
+                    Console.WriteLine("Wert für Alter zu groß (max. 50)");
+                }
+                else
+                {
+                    _alter = value;
+                }
+            }
+        }
+
+
+
+        // Methoden
+        // Methode, die Wuff auf der Konsole ausgibt
+        public void Bellen()
+        {
+            Console.WriteLine($"Der Hund {_name} bellt!");
+            Console.WriteLine("Wuff!!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using CsTutorial;
+
+// Deklaration und Initialisierung
+Hund bello = new Hund();
+//bello.Name = "";    // Test Name Leerstring
+//bello.Name = null;  // Test Name null (keine Referenz)
+bello.Name = "Bello";
+//bello.Alter = 51; // Test zu großer Wert
+//bello.Alter = -1; // Test zu kleiner Wert
+bello.Alter = 3;
+
+
+// Ausgabe
+bello.Bellen();
+Console.WriteLine($"Das Alter von {bello.Name} ist {bello.Alter}.");
